e22e2eb9400a9513902ffc767555aaa4c9464ce5
marcelvaldhano <marcelvaldhano@gmail.com>
2023-09-09 22:25:12 +0700

    feat: add besttimetobuyandsellstock II solution

---
+++ 122-BesTimeToBuyAndSellStock II/besttimetobuyandsellstock.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+	profit := 0
+	min := prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < min {
+			min = prices[i]
+		} else if prices[i]-min > 0 {
+			profit += prices[i] - min
+			min = prices[i]
+		}
+	}
+	return profit
+}
+func main() {
+	price := []int{7, 1, 5, 3, 6, 4}
+	fmt.Println(maxProfit(price))
+	price = []int{1, 2, 3, 4, 5}
+	fmt.Println(maxProfit(price))
+}
