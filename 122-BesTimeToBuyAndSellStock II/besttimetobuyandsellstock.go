0a238ca9ec34cfc125fb1c89dde338c8564a4e07
marcelvaldhano <marcelvaldhano@gmail.com>
2023-09-09 22:42:22 +0700

    feat:add other solution for besttimetobuyandsellstock II

--- 122-BesTimeToBuyAndSellStock II/besttimetobuyandsellstock.go
+++ 122-BesTimeToBuyAndSellStock II/besttimetobuyandsellstock.go
@@ -16,6 +16,18 @@ func maxProfit(prices []int) int {
 	}
 	return profit
 }
+
+func maxProfit2(prices []int) int {
+	profit := 0
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			profit += prices[i] - prices[i-1]
+		}
+	}
+	return profit
+}
+
 func main() {
 	price := []int{7, 1, 5, 3, 6, 4}
 	fmt.Println(maxProfit(price))
