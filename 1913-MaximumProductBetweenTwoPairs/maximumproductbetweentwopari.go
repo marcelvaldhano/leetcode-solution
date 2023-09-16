dfe46ec0e9d69bc910e2b28699f134239ec035c2
marcelvaldhano <marcelvaldhano@gmail.com>
2023-09-17 00:50:35 +0700

    feat:add solution for Maximum Product Difference Between Two Pairs

--- 1913-MaximumProductBetweenTwoPairs/maximumproductbetweentwopari.go
+++ 1913-MaximumProductBetweenTwoPairs/maximumproductbetweentwopari.go
@@ -5,29 +5,27 @@ import "fmt"
 func maxProductDifference(nums []int) int {
 	max := make([]int, 2)
 	min := make([]int, 2)
-	min[0], min[1] = nums[0], nums[0]
+	min[0], min[1] = 10001, 10001
 
 	for i := range nums {
-		if nums[i] >= max[1] {
+		if nums[i] > max[1] {
 			max[1] = nums[i]
 			if max[1] > max[0] {
 				max[0], max[1] = max[1], max[0]
 			}
 		}
-		if nums[i] <= min[1] {
+		if nums[i] < min[1] {
 			min[1] = nums[i]
 			if min[1] < min[0] {
 				min[0], min[1] = min[1], min[0]
 			}
 		}
 	}
-	fmt.Println("maks: ", max[0], max[1])
-	fmt.Println("maks: ", min[0], min[1])
 	return (max[0] * max[1]) - (min[1] * min[0])
 }
 
-// catsandog
-
 func main() {
+	fmt.Println(maxProductDifference([]int{5, 6, 2, 7, 4}))
+	fmt.Println(maxProductDifference([]int{4, 2, 5, 9, 7, 4, 8}))
 	fmt.Println(maxProductDifference([]int{1, 6, 7, 5, 2, 4, 10, 6, 4}))
 }
