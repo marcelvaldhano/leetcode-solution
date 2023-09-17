56f5c8fafb99e3eee40ee46fa748376263367a57
marcelvaldhano <marcelvaldhano@gmail.com>
2023-09-17 15:07:34 +0700

    feat: add solution for spiral matrix problem

---
+++ 54-spiralmatrix/spiralmatrix.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func spiralOrder(matrix [][]int) []int {
+	top := 0
+	left := 0
+	buttom := len(matrix)
+	right := len(matrix[0])
+
+	res := []int{}
+
+	for top < buttom && left < right {
+		for i := left; i < right; i++ {
+			res = append(res, matrix[top][i])
+		}
+		top++
+		for i := top; i < buttom; i++ {
+			res = append(res, matrix[i][right-1])
+		}
+		right--
+
+		if left >= right || top >= buttom {
+			break
+		}
+
+		for i := right - 1; i >= left; i-- {
+			res = append(res, matrix[buttom-1][i])
+		}
+		buttom--
+		for i := buttom - 1; i >= top; i-- {
+			res = append(res, matrix[i][left])
+		}
+		left++
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(spiralOrder([][]int{[]int{1, 2, 3}, []int{4, 5, 6}, []int{7, 8, 9}}))
+	fmt.Println(spiralOrder([][]int{[]int{1, 2, 3, 4}, []int{5, 6, 7, 8}, []int{9, 10, 11, 12}}))
+}
