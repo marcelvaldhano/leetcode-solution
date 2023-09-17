a4e95f46af13bdb3f067b61e0076703edc07e2cf
marcelvaldhano <marcelvaldhano@gmail.com>
2023-09-17 15:08:17 +0700

    feat: add solution for spiral matrix problem

--- 54-spiralmatrix/spiralmatrix.go
+++ 54-spiralmatrix/spiralmatrix.go
@@ -37,6 +37,6 @@ func spiralOrder(matrix [][]int) []int {
 }
 
 func main() {
-	fmt.Println(spiralOrder([][]int{[]int{1, 2, 3}, []int{4, 5, 6}, []int{7, 8, 9}}))
-	fmt.Println(spiralOrder([][]int{[]int{1, 2, 3, 4}, []int{5, 6, 7, 8}, []int{9, 10, 11, 12}}))
+	fmt.Println(spiralOrder([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}))
+	fmt.Println(spiralOrder([][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}))
 }
